60cad14e320e72b1b02e18a3d381212a26ecea35
anitahua318 <sh4351@columbia.edu>
2023-01-13 08:23:23 -0500

    hi
    hi

---
+++ components/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 id ="header1">
+            Anita Hua
+        </h1>
+        <p>
+            Hi! I am Anita, a first year at Columbia SEAS! 
+        </p>
+        <a href="https://mp.weixin.qq.com/s?__biz=MzI2NTgwNDI3MA==&mid=2247485959&idx=1&sn=f4d15e5d5b4406ef3ee1f8008b1c89a3&chksm=ea968a86dde103907e8e6eb4e95cddaeb1064c90c8bd6b504d5014da5bc20332efd614997426&token=1530952013&lang=zh_CN#rd">link to my newest blog post on Columbia Uni Writing </a>
+        <img src="Web1.png"/>
+        <nuxt-link to="Weixin">My Writing Blog</nuxt-link>
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+#header1{
+    font-size: 100px;
+    font-family:Lobster;
+}
+</style>
